b31ff94c8a120d4c488edd4dc5438a0d752a0da7
Kudo Chien <kudo@expo.dev>
2023-10-19 01:40:51 +0800

    migrate to new hook api

--- packages/expo-plugin-tinybase-inspector/App.tsx
+++ packages/expo-plugin-tinybase-inspector/App.tsx
@@ -3,51 +3,52 @@ import { createStore } from 'tinybase/debug';
 import { Provider } from 'tinybase/debug/ui-react';
 import { StoreInspector } from 'tinybase/debug/ui-react-dom';
 
-// @ts-ignore
-import { connectPluginFromDevToolsAsync } from 'expo/devtools';
+import { useDevToolsPluginClient } from 'expo';
 
 const store = createStore();
-const client = connectPluginFromDevToolsAsync();
-
-async function getClientAsync() {
-  return await client;
-}
-
-async function sendEditsAsync() {
-  const client = await getClientAsync();
-  client.sendMessage('@tinybase-inspector/edit', store.getJson());
-}
 
 let __ignoreStoreUpdates = false;
 
 export default function App() {
+  const client = useDevToolsPluginClient('expo-plugin-tinybase-inspector');
+
   useEffect(() => {
-    async function initAsync() {
-      const client = await getClientAsync();
+    const subscriptions = [];
 
-      client.addMessageListener('@tinybase-inspector/init', (data) => {
+    subscriptions.push(
+      client?.addMessageListener('@tinybase-inspector/init', (data) => {
         __ignoreStoreUpdates = true;
         store.setJson(data);
         __ignoreStoreUpdates = false;
-      });
+      })
+    );
 
-      client.addMessageListener('@tinybase-inspector/update', (data) => {
+    subscriptions.push(
+      client?.addMessageListener('@tinybase-inspector/update', (data) => {
         __ignoreStoreUpdates = true;
         store.setJson(data);
         __ignoreStoreUpdates = false;
-      });
-    }
+      })
+    );
 
-    store.addDidFinishTransactionListener(() => {
+    const listenerId = store.addDidFinishTransactionListener(() => {
       if (__ignoreStoreUpdates) {
         return;
       }
-
-      sendEditsAsync();
+      client?.sendMessage('@tinybase-inspector/edit', store.getJson());
     });
 
-    initAsync();
-  }, []);
+    return () => {
+      store.delListener(listenerId);
+      for (const subscription of subscriptions) {
+        subscription?.remove();
+      }
+    };
+  }, [client, store]);
+
+  if (client == null) {
+    return null;
+  }
 
   return (
     <Provider store={store}>
